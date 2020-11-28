fd2dc1ab447c49261dad0e8951cf94903b3918f5
alvishspt <72914932+alvishspt@users.noreply.github.com>
2020-11-28 16:55:33 +0530

    changes on edit screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,6 +8,19 @@ const fontColor = Color(0xff1c2843);
 bool isMenuScreen = true;
 bool isEditScreen = false;
 
+const List<BoxShadow> boxShadowSmall = const [
+  BoxShadow(
+    color: Color(0xffe6e6e6),
+    offset: Offset(3, 3),
+    blurRadius: 4,
+  ),
+  BoxShadow(
+    color: Color(0xfffcfcfc),
+    offset: Offset(-3, -3),
+    blurRadius: 4,
+  ),
+];
+
 const List<BoxShadow> boxShadow = const [
   BoxShadow(
     color: Colors.grey,
